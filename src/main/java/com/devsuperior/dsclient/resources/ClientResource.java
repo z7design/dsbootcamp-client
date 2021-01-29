7f387dc5a272069c1b554820c85f2a8538e7f30e
Jose Mario Ramos <z7design@z7design.com.br>
2021-01-28 22:39:07 -0300

    Atualization ther ResourceClient pagination

--- src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -31,15 +31,14 @@ public class ClientResource {
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+				@RequestParam(value = "page", defaultValue = "0") Integer page,
+				@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+				@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+				@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+				
 			) {
 		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy );
-		
-		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy );		
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
